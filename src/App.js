8497bb97985ff1e7102814e739649cb77b952d94
Maria Guerrero <guerrermj@gmail.com>
2020-07-02 14:46:22 -0400

    updating context module. refactored code to fetch info from API

--- src/App.js
+++ src/App.js
@@ -8,13 +8,41 @@ import Note from "./Note/Note";
 import AddNoteForm from "./AddNoteForm/AddNoteForm";
 import * as uuid from "uuid";
 import MainNav from "./MainNav/MainNav";
+import { type } from "os";
+import APIContext from "./APIContext";
 
 class App extends Component {
   state = {
-    notes: this.props.notes,
-    folders: this.props.folders
+    notes: [],
+    folders: []
   };
 
+  componentDidMount() {
+    Promise.all([
+      fetch(`http://localhost:9090/folders`),
+      fetch(`http://localhost:9090/notes`)
+    ])
+      .then(responses => {
+        return Promise.all([responses[0].json(), responses[1].json()]);
+      })
+      .then(data => {
+        this.setState({
+          notes: data[1],
+          folders: data[0]
+        });
+      });
+  }
+
+  handleDeleteNote = noteId => {
+    fetch(`http://localhost:9090/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json"
+      }
+    }).then(() => {
+      this.componentDidMount();
+    });
+  };
   addNote = (e, routerProps) => {
     e.preventDefault();
     const { noteName, noteContent } = e.target;
@@ -29,56 +57,66 @@ class App extends Component {
     routerProps.history.push("/");
   };
   render() {
+    const value = {
+      notes: this.state.notes,
+      folders: this.state.folders,
+      deleteNote: this.handleDeleteNote
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1></h1>
-        </header>
-        <aside>
-          <Route
-            path="/"
-            render={renderProps => <MainNav folders={this.state.folders} />}
-          />
-        </aside>
-        <main>
-          <Route
-            exact
-            path="/"
-            render={renderProps => (
-              <NoteList folders={this.state.folders} notes={this.state.notes} />
-            )}
-          />
-          <Route path="/addfolder/" component={AddFolder} />
-          <Route
-            path="/folder/:folderid"
-            render={renderProps => (
-              <NoteList
-                folders={this.state.folders}
-                notes={this.state.notes.filter(
-                  note => note.folderId == renderProps.match.params.folderid
-                )}
-              />
-            )}
-          />
-          <Route
-            path="/note/:noteid"
-            render={renderProps => (
-              <Note
-                folders={this.state.folders}
-                note={this.state.notes.find(
-                  note => note.id == renderProps.match.params.noteid
-                )}
-              />
-            )}
-          />
-          <Route
-            path="/addnote/"
-            render={renderProps => (
-              <AddNoteForm addNote={this.addNote} renderProps={renderProps} />
-            )}
-          ></Route>
-        </main>
-      </div>
+      <APIContext.Provider value={value}>
+        <div className="App">
+          <header className="App-header">
+            <h1></h1>
+          </header>
+          <aside>
+            <Route
+              path="/"
+              render={renderProps => <MainNav folders={this.state.folders} />}
+            />
+          </aside>
+          <main>
+            <Route
+              exact
+              path="/"
+              render={renderProps => (
+                <NoteList
+                  folders={this.state.folders}
+                  notes={this.state.notes}
+                />
+              )}
+            />
+            <Route path="/addfolder/" component={AddFolder} />
+            <Route
+              path="/folder/:folderid"
+              render={renderProps => (
+                <NoteList
+                  folders={this.state.folders}
+                  notes={this.state.notes.filter(
+                    note => note.folderId == renderProps.match.params.folderid
+                  )}
+                />
+              )}
+            />
+            <Route
+              path="/note/:noteid"
+              render={renderProps => (
+                <Note
+                  folders={this.state.folders}
+                  note={this.state.notes.find(
+                    note => note.id == renderProps.match.params.noteid
+                  )}
+                />
+              )}
+            />
+            <Route
+              path="/addnote/"
+              render={renderProps => (
+                <AddNoteForm addNote={this.addNote} renderProps={renderProps} />
+              )}
+            ></Route>
+          </main>
+        </div>
+      </APIContext.Provider>
     );
   }
 }
--- src/Note/Note.js
+++ src/Note/Note.js
@@ -2,34 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
 import "./Note.css";
+import APIContext from "../APIContext";
 
-export default function Note(props) {
-  console.log(props);
-  return (
-    <div className="Note">
-      <h2 className="title">
-        <Link to={`/note/${props.note.id}`}>{props.note.name}</Link>
-      </h2>
-  <div className="note-content">{props.note.content}</div>
-      <button className="Note__delete" type="button">
-        {" "}
-        Delete Note
-      </button>
-      <div className="Note__dates">
-        <div className="Note__dates-modified">
-          Date modified{" "}
-          <span className="Date">
-            
-            {props.note.modified
-              ? format(new Date(props.note.modified), "Do MM yyyy")
-              : ""}
-          </span>
+class Note extends React.Component {
+  static contextType = APIContext;
+
+  render() {
+    return (
+      <div className="Note">
+        <h2 className="title">
+          <Link to={`/note/${this.props.note.id}`}>{this.props.note.name}</Link>
+        </h2>
+        <div className="note-content">{this.props.note.content}</div>
+        <button
+          className="Note__delete"
+          type="button"
+          onClick={e => this.context.deleteNote(this.props.note.id)}
+        >
+          {" "}
+          Delete Note
+        </button>
+        <div className="Note__dates">
+          <div className="Note__dates-modified">
+            Date modified{" "}
+            <span className="Date">
+              {this.props.note.modified
+                ? format(new Date(this.props.note.modified), "Do MM yyyy")
+                : ""}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
+export default Note;
 Note.defaultProps = {
   note: {}
 };
